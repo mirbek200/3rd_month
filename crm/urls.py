f24f50ca2d1d639bdcbb54b160a4fb96187f525d
mirbek1 <109955612+mirbek1@users.noreply.github.com>
2022-09-14 18:27:26 +0600

    the end

--- crm/urls.py
+++ crm/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
 from .views import (UserListView,
                     CategoryListApiView,
-                    )
+                    AvgPriceApiView,
+                    MinPriceApiView,
+                    MaxPriceApiView,
+                    PriceApiView,
+                    Revenue,
+                    LenProduct)
 
 urlpatterns = [
     path('user_list/', UserListView.as_view(), name='user-list'),
     path('cat_list/', CategoryListApiView.as_view(), name='cat-list'),
+    path('avg/',  AvgPriceApiView.as_view(), name='avg'),
+    path('min/',  MinPriceApiView.as_view(), name='min'),
+    path('max/',  MaxPriceApiView.as_view(), name='max'),
+    path('spatistic/',  PriceApiView.as_view(), name='statistic'),
+    path('revenue/',  Revenue.as_view(), name='revenue'),
+    path('len_product/', LenProduct.as_view(), name='products'),
+
 ]
+
+
--- crm/views.py
+++ crm/views.py
@@ -11,12 +11,20 @@ from user.serializers import UserSerializers, AccountSerializers
 from user.permissions import *
 
 
-class UserListView(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializers
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['username', 'email']
+class UserListView(APIView):
 
+    def get(self, request):
+        users = User.objects.all()
+        user_list = []
+        for i in users:
+            user_list.append(i.username)
+
+        username = len(user_list)
+
+        data = {
+            "users": username,
+        }
+        return Response(data)
 
 class CategoryListApiView(APIView):
 
@@ -26,3 +34,99 @@ class CategoryListApiView(APIView):
         return Response(serializers.data)
 
 
+class PriceApiView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        price_list = []
+        for i in products:
+            price_list.append(i.price)
+        max_price = max(price_list)
+        min_price = min(price_list)
+        avg_price = sum(price_list) / len(price_list)
+
+        data = {
+            "max_price": max_price,
+            "min_price": min_price,
+            "avg_price": avg_price,
+        }
+        return Response(data)
+
+
+class AvgPriceApiView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        price_list = []
+        for i in products:
+            price_list.append(i.price)
+        avg_price = sum(price_list) / len(price_list)
+
+        data = {
+            "avg_price": avg_price
+
+        }
+        return Response(data)
+
+
+class MinPriceApiView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        price_list = []
+        for i in products:
+            price_list.append(i.price)
+        min_price = min(price_list)
+
+        data = {
+            "min_price": min_price
+        }
+        return Response(data)
+
+
+class MaxPriceApiView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        price_list = []
+        for i in products:
+            price_list.append(i.price)
+        max_price = max(price_list)
+
+
+        data = {
+            "max_price": max_price
+        }
+        return Response(data)
+
+
+class Revenue(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        revenue = []
+        for i in products:
+            one = i.price * i.quantity
+            revenue.append(one)
+        revenue = sum(revenue)
+
+        data = {
+            "revenue": revenue
+        }
+        return Response(data)
+
+
+class LenProduct(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        posts = []
+        for i in products:
+            posts.append(i.id)
+
+        product = len(posts)
+
+        data = {
+            "product": product
+        }
+        return Response(data)
